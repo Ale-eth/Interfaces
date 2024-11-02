9d42412c54fe04b0b6ec5c87b010694fe5747b71
Ale <alejandroabdala2000@gmail.com>
2024-11-02 20:01:20 -0300

    timer fix

---
+++ Retrogemas/js/fix.js
@@ -0,0 +1,14 @@
+const container = document.getElementById("game-container-fix");
+
+container.style.backgroundColor = "rgb(39, 16, 16)";
+
+
+const button = document.getElementById("4enlinea");
+
+button.addEventListener("click", function() {
+    container.style.backgroundColor = "rgb(2, 9, 16)";
+});
+
+
+
+
